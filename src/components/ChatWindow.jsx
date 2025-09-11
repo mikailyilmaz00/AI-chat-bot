74c4cafda64b801c526416dd18a0ce6505465c20
mikailyilmaz00 <mikail17yz@gmail.com>
2025-09-11 17:03:22 +0200

    working on components

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
 import Message from "./Message";
-import chatInput from "./chatInput"
-import { text } from "express";
+import ChatInput from "./chatInput"
 
 export default function ChatWindow() {
-    const [messages, setMessage] = useState([
-        { text: "Hej, jeg er din AI-assistent.👋", role: "ai"}
+    const [messages, setMessages] = useState([
+        { text: "Hej, jeg er en chatrobot, der er her for at hjælpe dig.👋", role: "ai"}
     ])
-}
+
 const handleSend  = (newMessage) => {
     if (!newMessage.trim()) return;
-    
-}
+
+    const updated = [...messages, { text: newMessage, role: "User"}]
+    setMessages(updated);
+
+    setTimeout(() => {
+        setMessages((prev) => [
+            ...prev,
+            { text: "AI placeholder svar", role: "ai" }
+        ])
+    }, 500)
+    }
+
+return (
+    <div className="chat-window">
+        <div className="messages">
+            {messages.map((msg, index) => (
+                <Message key={index} text={msg.text} role={msg.role} />
+            ))}
+        </div>
+        <ChatInput onSend={handleSend} />
+    </div>
+
+        )
+    }
