430210227df2094d273f5193b766213334fe04c5
mikailyilmaz00 <mikail17yz@gmail.com>
2025-09-18 17:04:24 +0200

    working on components

--- src/components/speechInput.jsx
+++ src/components/speechInput.jsx
@@ -3,13 +3,13 @@ import { io } from "socket.io-client"
 
 
 function SpeechInput() {
-    const [text, setText] = useState()
-    const [username, setUsername] = useState()
-    const [message, setMessage] = useState()
-    const [recognitionRefef] = useRef()
-    const [socketRef] = useRef()
+    const [text, setText] = useState("")
+    const [username, setUsername] = useState("")
+    const [message, setMessage] = useState("")
+    const [recognitionRefef] = useRef("")
+    const [socketRef] = useRef("")
     const [isListening, setIsListening] = useState(false)
-    const [error, setError] = useState()
+    const [error, setError] = useState("")
     }
 
 
@@ -19,3 +19,20 @@ function SpeechInput() {
             setMessages((prev) => [...prev, message])
         })
     })
+
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition()
+    recognition.lang = "da-DK", "en-EN"
+    recognition.interimResults = false;
+
+
+    const recognitionHandler = (e) => {
+        const last = e.results.length - 1;
+        const transcript = e.results[last][0].transcript
+        setText(transcript)
+
+        if (socketRef.current) {
+            socketRef.current.emit("", transcript)
+        }
+    }
+    
